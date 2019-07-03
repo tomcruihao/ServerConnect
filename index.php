d0b9a83662035bb4cb338ae58ea456da18c4282d
tomcruihao <tomcruihao.chang@gmail.com>
2019-07-03 16:04:21 +0800

    Extract marc number

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
-  function getrealurl($url){
-      $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
-      if(!$header){
-          exit('无法打开此网站'.$url);
+  function getrealurl($url) {
+    $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
+    if (!$header) {
+        exit('无法打开此网站'.$url);
+    }
+    //var_dump($header);
+    if (strpos($header[0],'301') || strpos($header[0],'302')) {
+      if(is_array($header['Location'])) {
+        return $header['Location'][count($header['Location'])-1];
+      }else{
+        return $header['Location'];
       }
-      //var_dump($header);
-      if (strpos($header[0],'301') || strpos($header[0],'302')) {
-          if(is_array($header['Location'])) {
-              return $header['Location'][count($header['Location'])-1];
-          }else{
-              return $header['Location'];
-          }
-      }else {
-          return $url;
-      }
+    } else {
+      return $url;
+    }
   }
 
-  $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
+  // $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
+  $url = $_GET['url'];
   $url = getrealurl($url);
-  echo '真实的url为：'.$url;
-?>  
+  $marc = parse_url($url, PHP_URL_PATH);
+  echo '真实的marc为：'.$marc;
+?>
