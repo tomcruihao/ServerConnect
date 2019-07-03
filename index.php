c5259f8a0fe80f35e48c98787a5f6e72ecb35eee
tomcruihao <tomcruihao.chang@gmail.com>
2019-07-03 16:47:18 +0800

    test

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+
   function getrealurl($url){
       $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
       if(!$header){
@@ -19,6 +21,8 @@
   $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
   $url = getrealurl($url);
   $url_extract = explode("=",$url);
-  echo $url_extract;
+  echo $url_extract[1];
+
+  // http://opac.lib.nankai.edu.cn/opac/ajax_item.php?marc_no=44737544646d47744264694d7350795a725538426f413d3d
   // echo '真实的url为：'.$url;
 ?>
