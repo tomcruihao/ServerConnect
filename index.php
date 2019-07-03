85ea215a8c6522fbb95ed30e81f443c4eedb0c36
tomcruihao <tomcruihao.chang@gmail.com>
2019-07-03 17:29:10 +0800

    Change the mode to use the parameter to sent value

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   header("Access-Control-Allow-Origin: *");
   header("Content-Type:text/html; charset=utf-8");
 
-  // $getOriginalUrl = $_GET['oriurl'];
+  $getOriginalUrl = $_GET['oriurl'];
 
   function getrealurl($url){
     $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
@@ -21,8 +21,8 @@
     }
   }
 
-  $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
-  $url = getrealurl($url);
+  // $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
+  $url = getrealurl($getOriginalUrl);
   $url_extract = explode("=",$url);
 
   echo file_get_contents("http://opac.lib.nankai.edu.cn/opac/ajax_item.php?marc_no=".$url_extract[1]);
