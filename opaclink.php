1fe9812cd45f8566632d4d8700abb44a1ee5c4a9
tomcruihao <tomcruihao.chang@gmail.com>
2019-07-31 11:57:30 +0800

    change

--- opaclink.php
+++ opaclink.php
@@ -8,15 +8,12 @@
   $connectingUrl = 'http://opac.lib.nankai.edu.cn/api/itemgo.php'
 
 
-  $timestamp = getCurrentTime();
-  echo timestamp
-  $stringBinding = marcNum.timestamp.appKey;
+  // $timestamp = getCurrentTime();
+  // echo timestamp
+  // $stringBinding = marcNum.timestamp.appKey;
 
-  // const sign = md5(stringBinding);
-  // return `${connectUrl}?marc_no=${marcNum}&appid=${appID}&time=${timestamp}&sign=${sign}`;
-
-  function getCurrentTime() {
-    $currentTime = time();
-    return date("d-m-Y h:i:s", $currentTime);
-  }
+  // function getCurrentTime() {
+  //   $currentTime = time();
+  //   return date("d-m-Y h:i:s", $currentTime);
+  // }
 ?>
