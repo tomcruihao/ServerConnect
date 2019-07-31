1ad1fcfbea5d5eac3959a7116bd5a5ab8dfcfa8b
tomcruihao <tomcruihao.chang@gmail.com>
2019-07-31 12:03:03 +0800

    add commend

--- opaclink.php
+++ opaclink.php
@@ -6,19 +6,19 @@
   $marcNum = $_GET['marc'];
   
   // set parameter
-  $appID = 'eds'
-  $appKey = 'z18gEZ0bzPMeGpai'
-  $connectingUrl = 'http://opac.lib.nankai.edu.cn/api/itemgo.php'
+  $appID = 'eds';
+  $appKey = 'z18gEZ0bzPMeGpai';
+  $connectingUrl = 'http://opac.lib.nankai.edu.cn/api/itemgo.php';
 
 
-  // $timestamp = getCurrentTime();
-  // echo timestamp
-  // $stringBinding = marcNum.timestamp.appKey;
+  $timestamp = getCurrentTime();
+  echo timestamp;
+  $stringBinding = marcNum.timestamp.appKey;
 
-  // function getCurrentTime() {
-  //   $currentTime = time();
-  //   return date("d-m-Y h:i:s", $currentTime);
-  // }
+  function getCurrentTime() {
+    $currentTime = time();
+    return date("d-m-Y h:i:s", $currentTime);
+  }
 
 
   // $getOriginalUrl = $_GET['oriurl'];
