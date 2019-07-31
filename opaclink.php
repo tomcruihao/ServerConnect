871d143229313ec74d167b40156a4b4877bbb7a7
tomcruihao <tomcruihao.chang@gmail.com>
2019-07-31 16:41:24 +0800

    change output format

--- opaclink.php
+++ opaclink.php
@@ -18,10 +18,8 @@
 
 
   $sign = md5($stringBinding);
-  echo $connectingUrl.'?marc_no='.$marcNum.'&appid='.$appID.'&time='.$timestamp.'&sign='.$sign;
-
-
-  http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-07-3115:49:40&sign=056edfadbaae78783c447481985fb744
+  
+  echo file_get_contents($connectingUrl.'?marc_no='.$marcNum.'&appid='.$appID.'&time='.$timestamp.'&sign='.$sign);
 
   // return `${connectUrl}?marc_no=${marcNum}&appid=${appID}&time=${timestamp}&sign=${sign}`;
 
