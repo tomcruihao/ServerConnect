54372506f6fe9d3e77a779b2d40d99d899f882b2
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-02 17:17:23 +0800

    est

--- test.php
+++ test.php
@@ -33,9 +33,9 @@
 
   function getUnivInfo() {
     // json object.
-    $univInfo = {"universities": [{"id": "s1213459", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.nankai.edu.cn/api/itemgo.php"},
+    $univInfo = json_decode('{"universities": [{"id": "s1213459", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.nankai.edu.cn/api/itemgo.php"},
       {"id": "29", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.hit.edu.cn/api/itemgo.php"}
-    ]};
+    ]}', true);
     echo $univInfo;
     // $univInfo = {"universities": [
     //   {"id": "s1213459", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.nankai.edu.cn/api/itemgo.php"},
