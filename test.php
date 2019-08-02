dfcddbc7ce78fb80c7e2bbde55cd96c34d1072ec
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-02 17:14:46 +0800

    check

--- test.php
+++ test.php
@@ -36,6 +36,7 @@
     $univInfo = json_decode('{"universities": [{"id": "s1213459", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.nankai.edu.cn/api/itemgo.php"},
       {"id": "29", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.hit.edu.cn/api/itemgo.php"}
     ]}', true);
+    echo $univInfo;
     // $univInfo = {"universities": [
     //   {"id": "s1213459", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.nankai.edu.cn/api/itemgo.php"},
     //   {"id": "29", "appID": "eds", "appKey": "z18gEZ0bzPMeGpai", "connectingUrl": "http://opac.lib.hit.edu.cn/api/itemgo.php"}
