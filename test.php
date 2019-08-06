6d9d4bbd3d6c6d1c22f7fd9c081e33923e53b240
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-06 14:14:31 +0800

    print

--- test.php
+++ test.php
@@ -3,24 +3,24 @@
   header("Content-Type:text/html; charset=utf-8");
 
   $getOriginalUrl = $_GET['oriurl'];
-  echo $getOriginalUrl;
 
-  function getrealurl($url){
-    $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
-    if(!$header){
-      exit('无法打开此网站'.$url);
-    }
-    //var_dump($header);
-    if (strpos($header[0],'301') || strpos($header[0],'302')) {
-      if(is_array($header['Location'])) {
-        return $header['Location'][count($header['Location'])-1];
-      } else {
-        return $header['Location'];
-      }
-    } else {
-      return $url;
-    }
-  }
+
+  // function getrealurl($url){
+  //   $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
+  //   if(!$header){
+  //     exit('无法打开此网站'.$url);
+  //   }
+  //   //var_dump($header);
+  //   if (strpos($header[0],'301') || strpos($header[0],'302')) {
+  //     if(is_array($header['Location'])) {
+  //       return $header['Location'][count($header['Location'])-1];
+  //     } else {
+  //       return $header['Location'];
+  //     }
+  //   } else {
+  //     return $url;
+  //   }
+  // }
 
   function newGetrealurl($url) {
     $ch = curl_init();
@@ -63,7 +63,9 @@
 
   // $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
   $url = newGetrealurl($getOriginalUrl);
+  echo $url;
   $url_extract = explode("=",$url);
+  echo $url_extract;
 
   echo file_get_contents("http://opac.lib.hit.edu.cn/opac/item.php?marc_no=".$url_extract[1]);
   // echo $url_extract[1];
