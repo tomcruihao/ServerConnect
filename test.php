45c7a1b9d2cc210fbd5f428fba35fb32da6900c3
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-06 14:05:31 +0800

    Change the otherway

--- test.php
+++ test.php
@@ -23,25 +23,40 @@
   }
 
   function newGetrealurl($url) {
-    $curl = curl_init();
-    curl_setopt_array($curl, array(    
-      CURLOPT_URL => $url,
-      CURLOPT_HEADER => true,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_NOBODY => true));
+    $ch = curl_init();
 
-    $header = explode("\n", curl_exec($curl));
-    curl_close($curl);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 
-    if (strpos($header[0],'301') || strpos($header[0],'302')) {
-      if(is_array($header['Location'])) {
-        return $header['Location'][count($header['Location'])-1];
-      } else {
-        return $header['Location'];
-      }
-    } else {
-      return $url;
-    }
+    $html = curl_exec($ch);
+
+    $redirectedUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+
+    curl_close($ch);
+
+    echo "Original URL:   " . $url . "\n";
+    echo "Redirected URL: " . $redirectedUrl . "\n";
+    return $redirectedUrl;
+    // $curl = curl_init();
+    // curl_setopt_array($curl, array(    
+    //   CURLOPT_URL => $url,
+    //   CURLOPT_HEADER => true,
+    //   CURLOPT_RETURNTRANSFER => true,
+    //   CURLOPT_NOBODY => true));
+
+    // $header = explode("\n", curl_exec($curl));
+    // curl_close($curl);
+
+    // if (strpos($header[0],'301') || strpos($header[0],'302')) {
+    //   if(is_array($header['Location'])) {
+    //     return $header['Location'][count($header['Location'])-1];
+    //   } else {
+    //     return $header['Location'];
+    //   }
+    // } else {
+    //   return $url;
+    // }
   }
 
 
