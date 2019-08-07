5fb61b7908b52721e78f441b1293b62df3dc1fa1
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-07 14:11:39 +0800

    integrate huiwen

---
+++ huiwen/opac.php
@@ -0,0 +1,49 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type:text/html; charset=utf-8");
+
+  date_default_timezone_set('Asia/Taipei');
+
+  $jsonFilePath = './test.json';
+
+  $sid = $_GET['sid'];
+  $marcNum = $_GET['marc'];
+
+  $univInfo = getJson($jsonFilePath);
+
+  // get Univ detail
+  $appID = null;
+  $appKey = null;
+  $connectingUrl = null;
+
+  foreach ($univInfo as $univ) {
+    if(strcasecmp($univ['id'], $sid) == 0) {
+      $appID = $univ['appID'];
+      $appKey = $univ['appKey'];
+      $connectingUrl = $univ['connectingUrl'];
+      break;
+    }
+  }
+
+  // generate url path
+  $timestamp = getCurrentTime();
+  $stringBinding = $marcNum.$timestamp.$appKey;
+
+  $sign = md5($stringBinding);
+  
+
+  echo $connectingUrl.'?marc_no='.$marcNum.'&appid='.$appID.'&time='.$timestamp.'&sign='.$sign;
+
+
+  function getJson($path) {
+    $getJsonData = file_get_contents($path);
+    $decodeJsonData = json_decode($getJsonData, true);
+
+    return $decodeJsonData;
+  }
+
+  function getCurrentTime() {
+    $currentTime = time();
+    return trim(date("Y-m-dH:i:s", $currentTime), ' ');
+  }
+?>
