b6cecdf6e55284234e9d59994e5249f0230ab672
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-08 11:04:29 +0800

    change formate

--- huiwen/add.php
+++ huiwen/add.php
@@ -1,9 +1,10 @@
 <?php
   header("Access-Control-Allow-Origin: *");
-  header("Content-Type:text/html; charset=utf-8");
+  header('Content-Type: application/json');
 
   $appKey = $_POST['appKey'];
   $appID = $_POST['appID'];
   $connectUrl = $_POST['connectUrl'];
-  echo $appKey.' '.$appID.' '.$connectUrl;
+  $data = array('appKey' => $appKey, 'appID' => $appID, 'connectUrl' => $connectUrl), JSON_NUMERIC_CHECK;
+  echo json_encode($data);
 ?>
