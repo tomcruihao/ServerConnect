bad725057b70b11b4d15c23ad719bf667bc4e712
tomcruihao <tomcruihao.chang@gmail.com>
2019-08-08 15:48:15 +0800

    add sid

--- huiwen/add.php
+++ huiwen/add.php
@@ -2,9 +2,34 @@
   header("Access-Control-Allow-Origin: *");
   header('Content-Type: application/json');
 
+  $sid = $_POST['sid'];
   $appKey = $_POST['appKey'];
   $appID = $_POST['appID'];
   $connectUrl = $_POST['connectUrl'];
-  $data = array('appKey' => $appKey, 'appID' => $appID, 'connectUrl' => $connectUrl);
-  echo json_encode($data, JSON_NUMERIC_CHECK);
+
+  $getJsonData = file_get_contents('./univ.json');
+  $decodeJsonData = json_decode($getJsonData, true);
+
+  $checkExist = false;
+
+  foreach ($getJsonData as $univ) {
+    if(strcasecmp($univ['id'], $sid) == 0) {
+      $checkExist = true;
+      break;
+    }
+  }
+
+  if(!checkExist) {
+    $newItem->id = $sid;
+    $newItem->appID = $appKey;
+    $newItem->appKey = $appID;
+    $newItem->connectingUrl = $connectUrl;
+    array_push($decodeJsonData, $newItem);
+    echo json_encode($decodeJsonData, JSON_NUMERIC_CHECK);
+  }
+
+
+  // return json value
+  // $data = array('appKey' => $appKey, 'appID' => $appID, 'connectUrl' => $connectUrl);
+  // echo json_encode($data, JSON_NUMERIC_CHECK);
 ?>
