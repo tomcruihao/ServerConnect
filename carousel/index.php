6581de29bdd6a80d1101a0e95636afe38d83ae24
tomcruihao <tomcruihao>
2019-10-23 17:59:56 +0800

    test

--- carousel/index.php
+++ carousel/index.php
@@ -35,6 +35,7 @@
     $xml = curl_exec($ch);
     $parseXml = simplexml_load_string($xml);
     curl_close($ch);
+    print_r($parseXml->SearchResults->records);
     // echo $parseXml->SearchResults->records->rec->plink;
     foreach($parseXml->SearchResults->records->rec->children() as $rec) {
       echo $rec->plink."<br>";
