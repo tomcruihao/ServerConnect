8cd4a29d0e9c7cba08aeacb5e840856a6364d0b3
tomcruihao <tomcruihao>
2019-10-23 18:33:57 +0800

    test

--- carousel/index.php
+++ carousel/index.php
@@ -43,7 +43,7 @@
       parse_str($parseUrlParam['query'], $query);
       $parts = parse_url($url);
       $AN = $query['AN'];
-      echo '<img src="https://rps2images.ebscohost.com/rpsweb/othumb?id=NL%'.$AN.'%24PDF&s=L"><br>';
+      echo '<img src="http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$PDF&s=l"><br>';
       // https://rps2images.ebscohost.com/rpsweb/othumb?id=NL%24108391%24PDF&s=l
     }
     // var_dump(json_decode($output, true));
