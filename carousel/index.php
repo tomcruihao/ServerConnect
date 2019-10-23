17d33f53aa13346eaaa5af827296b4dfb65dedd6
tomcruihao <tomcruihao>
2019-10-23 17:13:23 +0800

    test

--- carousel/index.php
+++ carousel/index.php
@@ -34,7 +34,7 @@
     // $xml = curl_exec($ch);
     $xml = simplexml_load_string(curl_exec($ch));
     curl_close($ch);
-    echo $xml;
+    echo $xml->plink[0];
     // var_dump(json_decode($output, true));
     // $decodeVal = json_decode($output, true);
     // print_r($decodeVal);
