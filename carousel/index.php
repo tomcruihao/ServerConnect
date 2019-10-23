648b437686078c332d1e738411a3c95c16f2219c
tomcruihao <tomcruihao>
2019-10-23 10:36:24 +0800

    test

--- carousel/index.php
+++ carousel/index.php
@@ -7,7 +7,6 @@
 
   // get the length of list
   // $elementCount  = count($bookList);
-  echo "total: ".$bookList;
 
   // foreach ($univInfo as $univ) {
   //   if(strcasecmp($univ['id'], $sid) == 0) {
@@ -18,14 +17,14 @@
   //   }
   // }
 
-  // create curl resource
-  $ch = curl_init();
-  // set url 
-  curl_setopt($ch, CURLOPT_URL, "https://eit.ebscohost.com/Services/SearchService.asmx/Search?prof=tylee.main.eit&&pwd=ebs3705&db=edsebk&query=IB+9780195141832");
-  // $output contains the output json
-  $output = curl_exec($ch);
-  // close curl resource to free up system resources 
-  curl_close($ch);
-  // {"name":"Baron","gender":"male","probability":0.88,"count":26}
-  var_dump(json_decode($output, true));
+  // // create curl resource
+  // $ch = curl_init();
+  // // set url 
+  // curl_setopt($ch, CURLOPT_URL, "https://eit.ebscohost.com/Services/SearchService.asmx/Search?prof=tylee.main.eit&&pwd=ebs3705&db=edsebk&query=IB+9780195141832");
+  // // $output contains the output json
+  // $output = curl_exec($ch);
+  // // close curl resource to free up system resources 
+  // curl_close($ch);
+  // // {"name":"Baron","gender":"male","probability":0.88,"count":26}
+  // var_dump(json_decode($output, true));
 ?>
