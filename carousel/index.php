5f31d7f206f19f4e3f622b4a3bf8be31d44668c7
tomcruihao <tomcruihao>
2019-10-23 16:25:15 +0800

    test

--- carousel/index.php
+++ carousel/index.php
@@ -30,7 +30,7 @@
     $apiUrl = "https://eit.ebscohost.com/Services/SearchService.asmx/Search?prof=tylee.main.eit&&pwd=ebs3705&db=edsebk&query=".$queryContent;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $apiUrl);
-    // $output = simplexml_load_string(curl_exec($ch));
+    $output = simplexml_load_string(curl_exec($ch));
     // // echo $output->plink[0];
     // curl_close($ch);
     // var_dump(json_decode($output, true));
