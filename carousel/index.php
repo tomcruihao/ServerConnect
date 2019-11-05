68c57f33baa3460be97adab5cc72278b5616f51e
tomcruihao <tomcruihao>
2019-11-05 13:35:35 +0800

    fix

--- carousel/index.php
+++ carousel/index.php
@@ -17,7 +17,7 @@
 
   $bookInfoList = getBookInfoFromServer($apiConnection, $randomBooklist);
 
-  // echo json_encode($bookInfoList, JSON_NUMERIC_CHECK);
+  echo json_encode($bookInfoList, JSON_NUMERIC_CHECK);
 
   function getBookInfoFromServer($apiUrl, $booklist) {
     $result = array();
