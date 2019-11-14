10984712703f680b124e64e6741c2157ff715263
tomcruihao <tomcruihao>
2019-11-14 17:32:57 +0800

    test

--- carouselQuery/index.php
+++ carouselQuery/index.php
@@ -14,31 +14,32 @@
 
   function getBookInfoFromServer($apiUrl, $keyword) {
     $result = array();
+    echo $keyword;
 
     // get value from API
     $apiUrl = $apiUrl."&query=".$keyword;
     
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    // $ch = curl_init();
+    // curl_setopt($ch, CURLOPT_URL, $apiUrl);
+    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 
-    $xml = curl_exec($ch);
-    $parseXml = simplexml_load_string($xml);
-    curl_close($ch);
+    // $xml = curl_exec($ch);
+    // $parseXml = simplexml_load_string($xml);
+    // curl_close($ch);
 
-    foreach($parseXml->SearchResults->records->children() as $rec) {
-      // get url and parse
-      $parseUrlParam = parse_url($rec->plink);
-      parse_str($parseUrlParam['query'], $query);
-      $parts = parse_url($url);
-      $AN = $query['AN'];
-      $imgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$PDF&s=l';
-      $directionUrl = 'http://search.ebscohost.com/login.aspx?direct=true&db=nlebk&AN='.$AN.'&site=ehost-live&custid=s1213459&authtype=ip,uid&groupid=main&profileid=ehost&scope=site';
-      $onErrorImgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$EPUB&s=l';
-      $title = $rec->header->controlInfo->bkinfo->btl;
-      $tempItem = array('title' => strval($title), 'imgUrl' => strval($imgUrl), 'directionUrl' => strval($directionUrl), 'onErrorUrlImg' => strval($onErrorImgUrl));
-      array_push($result, $tempItem);
-    }
+    // foreach($parseXml->SearchResults->records->children() as $rec) {
+    //   // get url and parse
+    //   $parseUrlParam = parse_url($rec->plink);
+    //   parse_str($parseUrlParam['query'], $query);
+    //   $parts = parse_url($url);
+    //   $AN = $query['AN'];
+    //   $imgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$PDF&s=l';
+    //   $directionUrl = 'http://search.ebscohost.com/login.aspx?direct=true&db=nlebk&AN='.$AN.'&site=ehost-live&custid=s1213459&authtype=ip,uid&groupid=main&profileid=ehost&scope=site';
+    //   $onErrorImgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$EPUB&s=l';
+    //   $title = $rec->header->controlInfo->bkinfo->btl;
+    //   $tempItem = array('title' => strval($title), 'imgUrl' => strval($imgUrl), 'directionUrl' => strval($directionUrl), 'onErrorUrlImg' => strval($onErrorImgUrl));
+    //   array_push($result, $tempItem);
+    // }
 
     return $result;
   }
