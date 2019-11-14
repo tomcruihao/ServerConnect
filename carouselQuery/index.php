d63e85e85e5332c4afbb6013020f1a7e7b31e6b2
tomcruihao <tomcruihao>
2019-11-14 17:36:54 +0800

    test

--- carouselQuery/index.php
+++ carouselQuery/index.php
@@ -26,19 +26,19 @@
     $parseXml = simplexml_load_string($xml);
     curl_close($ch);
 
-    foreach($parseXml->SearchResults->records->children() as $rec) {
-      // get url and parse
-      $parseUrlParam = parse_url($rec->plink);
-      parse_str($parseUrlParam['query'], $query);
-      $parts = parse_url($url);
-      $AN = $query['AN'];
-      $imgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$PDF&s=l';
-      $directionUrl = 'http://search.ebscohost.com/login.aspx?direct=true&db=nlebk&AN='.$AN.'&site=ehost-live&custid=s1213459&authtype=ip,uid&groupid=main&profileid=ehost&scope=site';
-      $onErrorImgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$EPUB&s=l';
-      $title = $rec->header->controlInfo->bkinfo->btl;
-      $tempItem = array('title' => strval($title), 'imgUrl' => strval($imgUrl), 'directionUrl' => strval($directionUrl), 'onErrorUrlImg' => strval($onErrorImgUrl));
-      array_push($result, $tempItem);
-    }
+    // foreach($parseXml->SearchResults->records->children() as $rec) {
+    //   // get url and parse
+    //   $parseUrlParam = parse_url($rec->plink);
+    //   parse_str($parseUrlParam['query'], $query);
+    //   $parts = parse_url($url);
+    //   $AN = $query['AN'];
+    //   $imgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$PDF&s=l';
+    //   $directionUrl = 'http://search.ebscohost.com/login.aspx?direct=true&db=nlebk&AN='.$AN.'&site=ehost-live&custid=s1213459&authtype=ip,uid&groupid=main&profileid=ehost&scope=site';
+    //   $onErrorImgUrl = 'http://rps2images.ebscohost.com/rpsweb/othumb?id=NL$'.$AN.'$EPUB&s=l';
+    //   $title = $rec->header->controlInfo->bkinfo->btl;
+    //   $tempItem = array('title' => strval($title), 'imgUrl' => strval($imgUrl), 'directionUrl' => strval($directionUrl), 'onErrorUrlImg' => strval($onErrorImgUrl));
+    //   array_push($result, $tempItem);
+    // }
 
     return $result;
   }
