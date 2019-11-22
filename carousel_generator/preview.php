1d7a986be4e96e9beada309ca4f3442107caccc6
tomcruihao <tomcruihao>
2019-11-22 17:23:09 +0800

    test

--- carousel_generator/preview.php
+++ carousel_generator/preview.php
@@ -1,10 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Slick Playground</title>
+  <meta charset="UTF-8">
+  </head>
+<body>
 <?php
   $base64Code = $_POST['code'];
   $pageCode = base64_decode($base64Code);
   echo $pageCode;
 ?>
-<style type="text/css">
-  section {
-    margin: 0;
-  }
-</style>
+</body>
+</html>
