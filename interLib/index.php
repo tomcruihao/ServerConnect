c8f3d1b1218b85850529a3552d216b2e4f1390d3
tomcruihao <tomcruihao>
2019-12-02 17:14:58 +0800

    test

--- interLib/index.php
+++ interLib/index.php
@@ -7,16 +7,16 @@
   $AN = $_GET['an'];
 
   $connectUrl = $domainUrl.'/holding?('.$term.')'.$AN;
-  echo $connectUrl;
+  getContent($connectUrl);
   // getContent('http://dev.jiatu.info:4000/holding?(szl)1005891840');
 
-  // function getContent($url) {
-  //   $ch1 = curl_init();
-  //   curl_setopt($ch1,CURLOPT_URL,$url);
-  //   curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
-  //   curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
-  //   $response = curl_exec($ch1);
-  //   curl_close($ch1);
-  //   echo $response;
-  // }
+  function getContent($url) {
+    $ch1 = curl_init();
+    curl_setopt($ch1,CURLOPT_URL,$url);
+    curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
+    $response = curl_exec($ch1);
+    curl_close($ch1);
+    echo $response;
+  }
 ?>
