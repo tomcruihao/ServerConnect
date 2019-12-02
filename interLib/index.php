4061b3d75eed9ce6aa522e20372201c1ccec41df
tomcruihao <tomcruihao>
2019-12-02 16:10:48 +0800

    test

--- interLib/index.php
+++ interLib/index.php
@@ -2,17 +2,21 @@
   header("Access-Control-Allow-Origin: *");
   header("Content-Type:text/html; charset=utf-8");
 
-  // $getOriginalUrl = $_GET['oriurl'];
+  $domainUrl = $_POST['oriurl'];
+  $term = $_POST['term'];
+  $AN = $_POST['an'];
 
-  getContent('http://dev.jiatu.info:4000/holding?(szl)1005891840');
-  
-  function getContent($url) {
-    $ch1 = curl_init();
-    curl_setopt($ch1,CURLOPT_URL,$url);
-    curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
-    $response = curl_exec($ch1);
-    curl_close($ch1);
-    echo $response;
-  }
+  $connectUrl = $domainUrl.'/holding?('.$term.')'.$AN;
+  echo $connectUrl;
+  // getContent('http://dev.jiatu.info:4000/holding?(szl)1005891840');
+
+  // function getContent($url) {
+  //   $ch1 = curl_init();
+  //   curl_setopt($ch1,CURLOPT_URL,$url);
+  //   curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
+  //   curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
+  //   $response = curl_exec($ch1);
+  //   curl_close($ch1);
+  //   echo $response;
+  // }
 ?>
