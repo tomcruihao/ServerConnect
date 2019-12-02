baad8b7043311e08c2ed4f746df6d626550204da
tomcruihao <tomcruihao>
2019-12-02 17:02:59 +0800

    test

--- interLib/index.php
+++ interLib/index.php
@@ -2,9 +2,9 @@
   header("Access-Control-Allow-Origin: *");
   header("Content-Type:text/html; charset=utf-8");
 
-  $domainUrl = $_POST['oriurl'];
-  $term = $_POST['term'];
-  $AN = $_POST['an'];
+  $domainUrl = $_GET['oriurl'];
+  $term = $_GET['term'];
+  $AN = $_GET['an'];
 
   $connectUrl = $domainUrl.'/holding?('.$term.')'.$AN;
   echo $connectUrl;
@@ -19,9 +19,4 @@
   //   curl_close($ch1);
   //   echo $response;
   // }
-?>
-<?php
-  $base64Code = $_POST['code'];
-  $pageCode = base64_decode($base64Code);
-  echo $pageCode;
 ?>
