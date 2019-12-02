2e95277c5d73147de9431553f6bd1fa7cde93963
tomcruihao <tomcruihao>
2019-12-02 15:59:43 +0800

    test

---
+++ interLib/index.php
@@ -0,0 +1,18 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type:text/html; charset=utf-8");
+
+  // $getOriginalUrl = $_GET['oriurl'];
+
+  getContent('http://dev.jiatu.info:4000/holding?(szl)1005891840');
+  
+  function getContent($url) {
+    $ch1 = curl_init();
+    curl_setopt($ch1,CURLOPT_URL,$url);
+    curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
+    $response = curl_exec($ch1);
+    curl_close($ch1);
+    echo $response;
+  }
+?>
