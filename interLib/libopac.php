8d57e807ed193d4678aaabc040d879950afffe9b
tomcruihao <tomcruihao>
2019-12-03 13:36:08 +0800

    add for lib opac

---
+++ interLib/libopac.php
@@ -0,0 +1,21 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type:text/html; charset=utf-8");
+
+  $domainUrl = $_GET['oriurl'];
+  $term = $_GET['term'];
+
+  $connectUrl = $domainUrl.'/opac/api/holding/'.$AN;
+
+  getContent($connectUrl);
+
+  function getContent($url) {
+    $ch1 = curl_init();
+    curl_setopt($ch1,CURLOPT_URL,$url);
+    curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
+    $response = curl_exec($ch1);
+    curl_close($ch1);
+    echo $response;
+  }
+?>
