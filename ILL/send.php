28e51f940704fa40cb2d4f63b395eb7bb44f5d0a
tomcruihao <tomcruihao>
2019-12-17 17:04:06 +0800

    test

--- ILL/send.php
+++ ILL/send.php
@@ -2,25 +2,36 @@
   header("Content-Type:text/html; charset=utf-8");
   date_default_timezone_set('Asia/Taipei');
 
-  $connectUrl = '';
+  $connectUrl = 'http://47.104.60.189:8085/services/WebService?wsdl';
 
-  $Name = $_POST['Name'];
-  $phone = $_POST['phone'];
+  $userName = $_POST['userName'];
+  $userPhone = $_POST['userPhone'];
+  $userEmail = $_POST['userEmail'];
 
-  array(
-    "libcode"=>$nm_patient,
-    "useremail"=>$id_patno,
-    "userphone"=>$st_sex,
-    "title"=>$dt_birth,
-    "magtitle"=>$st_age,
-    "mayyear"=>$id_patient,
-    "volnum"=>$id_bed,
-    "magnum"=>$txtInfo,
-    "pagenum"=>$imgInfo,
-    "systemid"=>$imgDraw,
-    "issn"=>$imgSign,
-    "doi"=>$imgSign
-  );
+  $jTitle = $_POST['title'];
+  $jMagtitle = $_POST['magtitle'];
+  $jMayyear = $_POST['mayyear'];
+  $jVolume = $_POST['volnum'];
+  $jMagnum = $_POST['magnum'];
+  $jPagenum = $_POST['pagenum'];
+  $jISSN = $_POST['issn'];
+  $jDOI = $_POST['doi'];
+
+  echo $jTitle.' '.$jMagtitle.' '.$jMayyear.' '.$jVolume.' '.$jMagnum.' '.$jPagenum.' '.$jISSN.' '.$jDOI;
+  // array(
+  //   "libcode"=>$nm_patient,
+  //   "useremail"=>$id_patno,
+  //   "userphone"=>$st_sex,
+  //   "title"=>$dt_birth,
+  //   "magtitle"=>$st_age,
+  //   "mayyear"=>$id_patient,
+  //   "volnum"=>$id_bed,
+  //   "magnum"=>$txtInfo,
+  //   "pagenum"=>$imgInfo,
+  //   "systemid"=>$imgDraw,
+  //   "issn"=>$imgSign,
+  //   "doi"=>$imgSign
+  // );
   function checkDataInfo($formInfo) {
     // required: libcode, useremail
     return 0;
