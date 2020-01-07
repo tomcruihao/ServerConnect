5cf6306c1b7874ac36c02e22351df9ad7d3725e3
tomcruihao <tomcruihao>
2020-01-07 10:51:33 +0800

    test

--- ILL/send1.php
+++ ILL/send1.php
@@ -3,50 +3,12 @@
   ini_set('display_errors', 1);
   header("Content-Type:text/html; charset=utf-8");
 
-  // $objSoapClient = new SoapClient("http://47.104.60.189:8085/services/WebService?wsdl");
-  // $param = array(
-  //   "libcode"=>"301000",
-  //   "useremail"=>"chchang@ebsco.com",
-  //   "userphone"=>"",
-  //   "title"=>"test",
-  //   "magtitle"=>"",
-  //   "issn"=>"",
-  //   "mayyear"=>"",
-  //   "volnum"=>"",
-  //   "magnum"=>"",
-  //   "pagenum"=>"",
-  //   "systemid"=>"3",
-  //   "doi"=>""
-  // );
-  // $out = $objSoapClient->ValidateZip($param);
-  // $data = $out->ValidateZipResult;
-  // echo $data;
-
 
   // Initialize WS with the WSDL
   $client = new SoapClient("http://47.104.60.189:8085/services/WebService?wsdl");
 
-  // Set request params
-  // $params = array(
-  //   "libcode"=>"301000",
-  //   "useremail"=>"chchang@ebsco.com",
-  //   "userphone"=>"",
-  //   "title"=>"test",
-  //   "magtitle"=>"",
-  //   "issn"=>"",
-  //   "mayyear"=>"",
-  //   "volnum"=>"",
-  //   "magnum"=>"",
-  //   "pagenum"=>"",
-  //   "systemid"=>"3",
-  //   "doi"=>""
-  // );
-  $params = array(
-    "{'libcode':'301000','useremail':'chchang@ebsco.com','title':'test','systemid':'3'}"
-  );
-
   // Invoke WS method (Function1) with the request params 
-  $response = $client->__soapCall("referReqmag", array($params));
+  $response = $client->__soapCall("referReqmag", array('{"libcode":"301000","useremail":"chchang@ebsco.com","title":"test","systemid":"3"}'));
 
   // Print WS response
   // var_dump($response);
