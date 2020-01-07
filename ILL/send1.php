f718840897cdb5125c1a7e2d9f32690f506d97af
tomcruihao <tomcruihao>
2020-01-07 10:19:52 +0800

    test

--- ILL/send1.php
+++ ILL/send1.php
@@ -1,6 +1,6 @@
 <?php
-  // error_reporting(E_ALL);
-  // ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
 
   // $objSoapClient = new SoapClient("http://47.104.60.189:8085/services/WebService?wsdl");
   // $param = array(
