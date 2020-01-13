5c958f066b06989f321c1fcc695545ee63dc58ea
tomcruihao <tomcruihao>
2020-01-13 13:46:46 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -23,6 +23,7 @@
       </thead>
       <tbody class="list" id="databaseList">
 <?php
+  echo $decodeJsonData['row'];
   foreach ($decodeJsonData['row'] as $row) {
     echo '<tr>';
     echo '<td class="id" style="display:none;">1</td>';
