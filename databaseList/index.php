8925ecae671346502f0da6125b668a03b4185ee5
tomcruihao <tomcruihao>
2020-01-13 11:45:26 +0800

    add php to get json

--- databaseList/index.php
+++ databaseList/index.php
@@ -4,7 +4,7 @@
   header('Content-Type: application/json; charset=utf-8');
 
 
-  echo {'test':'1234'};
+  echo "{'test':'1234'}";
   // echo file_get_contents('eResourceList.json');
   // $decodeJsonData = json_decode($getJsonData, true);
 ?>
