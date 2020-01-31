811d8038010149b80d72eb568f83fc171e547a2e
tomcruihao <tomcruihao>
2020-01-31 11:21:31 +0800

    test

--- databaseList/features/processResource.php
+++ databaseList/features/processResource.php
@@ -1,7 +1,7 @@
 <?php
-  ini_set('display_errors','1');
+  ini_set('display_errors',1);
   error_reporting(E_ALL);
-  
+
   header("Access-Control-Allow-Origin: *");
   header("Content-Security-Policy: upgrade-insecure-requests");
   header('Content-Type: application/json');
