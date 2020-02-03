2cd258c3b96f0206192cb9c6f807768e5d52ee11
tomcruihao <tomcruihao>
2020-02-03 16:59:43 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -233,7 +233,7 @@
         success: function(res) {
           console.log(res);
           self.bulletinTitle = res.bulletinTitle;
-          self.latestNewsList = res.newsList.sort((a, b) => a.publishDate - b.publishDate);
+          self.latestNewsList = res.newsList.slice().sort((a, b) => b.publishDate - a.publishDate);
         }
       });
     },
