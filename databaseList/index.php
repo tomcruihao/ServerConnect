939886d42be39d1be6ad8537d426eb7aa6dc361d
tomcruihao <tomcruihao>
2020-02-03 15:48:42 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -58,7 +58,7 @@
       echo '<tr>
               <td class="resourceName">
                 <div class="resourceName-title">'.$row['resourceName'].'</div>
-                <button class="direction" onclick="directTo(\''.$row['url'].'\')">
+                <button class="direction" onclick="directTo('.$row['id'].', \''.$row['url'].'\')">
                   <img src="img/direction.svg" alt="點我開啟" title="點我開啟"/>
                 </button>
               </td>
@@ -204,7 +204,8 @@
 <script type="text/javascript">
   var dataList = <?php echo $getJsonData; ?>;
   function directTo(url) {
-    console.log(url);
+    window.open(url, '_blank');
+    
   }
   function showDetail(info) {
     // show the dialogue
