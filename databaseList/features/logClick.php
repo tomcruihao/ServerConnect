46ebdf3e82c2a789a732ed4215e19ce7a466272d
tomcruihao <tomcruihao>
2020-02-05 17:48:36 +0800

    test

--- databaseList/features/logClick.php
+++ databaseList/features/logClick.php
@@ -4,7 +4,7 @@
   header('Content-Type: application/json');
   date_default_timezone_set('Asia/Taipei');
 
-  $getLogData = file_get_contents('../data/logUserCount.json');
+  $getLogData = file_get_contents('../data/logUserCountClick.json');
   $logData = json_decode($getLogData, true);
 
   // $receivedData = json_decode($_POST["directionData"], true);
@@ -13,17 +13,20 @@
   $log['clickedDateTime'] = date("Y-m-d h:i:s");
   $log['ip'] = getUserIpAddr();
 
-  print_r($log);
+  array_push($logData, $log);
+  // write back
+  file_put_contents('../data/logUserCountClick.json', json_encode($logData, JSON_UNESCAPED_UNICODE));
+  response('success', 'success');
 
   function getUserIpAddr(){
-    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-        //ip from share internet
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-        //ip pass from proxy
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }else{
-        $ip = $_SERVER['REMOTE_ADDR'];
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      //ip from share internet
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      //ip pass from proxy
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+      $ip = $_SERVER['REMOTE_ADDR'];
     }
     return $ip;
   }
