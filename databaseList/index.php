f0736a3d9bdf45a3f5e16c9434071ac2c42b7fc1
tomcruihao <tomcruihao>
2020-02-05 10:55:52 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -209,7 +209,7 @@
       </div>
     </div>
   </div>
-  <div class="mask" id="dialogue">
+  <div class="mask" id="dialogue" style="display: block;">
     <div class="dialogue-message-frame">
       <div class="dialogue-head">
         <img src="img/closeWhite.svg"/ class="close" @click="closeDialogue">
