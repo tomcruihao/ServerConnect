0deec2b72c3eb3dcccaf419915b0141fd19684fc
tomcruihao <tomcruihao>
2020-02-05 17:37:32 +0800

    test

--- databaseList/features/logClick.php
+++ databaseList/features/logClick.php
@@ -1,4 +1,20 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Security-Policy: upgrade-insecure-requests");
+  header('Content-Type: application/json');
+  date_default_timezone_set('Asia/Taipei');
+
+  $getLogData = file_get_contents('../data/logUserCount.json');
+  $logData = json_decode($getLogData, true);
+
+  // $receivedData = json_decode($_POST["directionData"], true);
+
+  $log = [];
+  $log['clickedDateTime'] = date("Y-m-d h:i:s");
+  $log['ip'] = getUserIpAddr();
+
+  print_r($log);
+
   function getUserIpAddr(){
     if(!empty($_SERVER['HTTP_CLIENT_IP'])){
         //ip from share internet
@@ -11,6 +27,4 @@
     }
     return $ip;
   }
-  echo 'date time - '.date("Y-m-d h:i:s");
-  echo 'User Real IP - '.getUserIpAddr();
 ?>
