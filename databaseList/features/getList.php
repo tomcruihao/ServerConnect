c6053b216d7425122f96c2867df95abf4464880e
tomcruihao <tomcruihao>
2020-02-08 12:06:39 +0800

    test

--- databaseList/features/getList.php
+++ databaseList/features/getList.php
@@ -3,6 +3,6 @@
   header("Content-Security-Policy: upgrade-insecure-requests");
   header('Content-Type: application/json');
 
-  $getJsonData = file_get_contents('../eResourceList.json');
+  $getJsonData = file_get_contents('../data/eResourceList.json');
   echo $getJsonData;
 ?>
