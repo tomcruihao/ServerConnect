07d35ff7e4ead6cb4b490e47ca9085544309dc3a
tomcruihao <tomcruihao>
2020-02-10 14:33:27 +0800

    test

--- databaseList/features/strokes.php
+++ databaseList/features/strokes.php
@@ -1,10 +1,10 @@
 <?php
   // get resource list
-  $getResourceListJsonData = file_get_contents('../eResourceList.json');
+  $getResourceListJsonData = file_get_contents('../data/eResourceList.json');
   $resourceList = json_decode($getResourceListJsonData, true);
 
   // get strokes list
-  $getStrokesJsonData = file_get_contents('../UniHanO.json');
+  $getStrokesJsonData = file_get_contents('../data/UniHanO.json');
   $strokes = json_decode($getStrokesJsonData, true);
 
   print_r($strokes);
