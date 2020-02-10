00619420eb580cc21e3fa8939c20e96107dda555
tomcruihao <tomcruihao>
2020-02-10 16:01:52 +0800

    test

--- databaseList/features/strokes.php
+++ databaseList/features/strokes.php
@@ -7,18 +7,16 @@
   $getStrokesJsonData = file_get_contents('../data/UniHanO.json');
   $strokes = json_decode($getStrokesJsonData, true);
 
-  foreach($strokes as $key => $row) {
-    $firstChar = $row['char'];
+  // foreach($strokes as $key => $row) {
+  //   $firstChar = $row['char'];
+  //   echo mb_detect_encoding($firstChar);
+  // }
+
+  foreach($resourceList['rows'] as $key => $row) {
+    $firstChar = substr($row['resourceName'], 0, 1);
     echo mb_detect_encoding($firstChar);
     // $newChar = iconv(mb_detect_encoding($firstChar), "big5", $firstChar);
 
     // echo $newChar;
   }
-
-  // foreach($resourceList['rows'] as $key => $row) {
-  //   $firstChar = substr($row['resourceName'], 0, 1);
-  //   $newChar = iconv(mb_detect_encoding($firstChar), "big5", $firstChar);
-
-  //   echo $newChar;
-  // }
 ?>
