c97f8ed10ee8931f15d31d80117c26546ba7ee9e
tomcruihao <tomcruihao>
2020-02-14 17:25:47 +0800

    test

---
+++ springer/redirect.php
@@ -0,0 +1,18 @@
+<?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type:text/html; charset=utf-8");
+
+  $isbn = $_GET['isbn'];
+  $postProcess_isbn = process_isbn($isbn);
+
+  function process_isbn($isbn) {
+    // 3-1-3-5-1
+    $temp = substr_replace($temp, '-', 11, 0);
+    $temp = substr_replace($temp, '-', 6, 0);
+    $temp = substr_replace($temp, '-', 3, 0);
+    $temp = substr_replace($temp, '-', 2, 0);
+
+    echo $temp;
+    return $temp;
+  }
+?>
