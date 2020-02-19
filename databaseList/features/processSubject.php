446444bd8bad25769a5dc98f9184dc391678cf28
tomcruihao <tomcruihao>
2020-02-19 17:56:28 +0800

    test

--- databaseList/features/processSubject.php
+++ databaseList/features/processSubject.php
@@ -17,7 +17,7 @@ error_reporting(E_ALL);
   // parameters
   $type = $_POST["type"];
   // $resource = $_POST["resource"];
-  $subjectList = json_decode($_POST["subject"], true);
+  // $subjectList = json_decode($_POST["subject"], true);
 
   if ($type === 'add') {
     // new object
