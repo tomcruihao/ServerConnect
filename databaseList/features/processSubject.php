2f45a4f6f7b78a4b38acac5b77bfa039ed47b0e6
tomcruihao <tomcruihao>
2020-02-19 17:53:52 +0800

    test

---
+++ databaseList/features/processSubject.php
@@ -0,0 +1,67 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
+
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Security-Policy: upgrade-insecure-requests");
+  // header("Content-Type:text/html;charset=utf-8");
+  header('Content-Type: application/json');
+
+  $jsonFile_direct = '../data/subject.json';
+
+  $getSubjectData = file_get_contents('../data/subject.json');
+  $subjectList = json_decode($getSubjectData, true);
+
+  // parameters
+  $type = $_POST["type"];
+  // $resource = $_POST["resource"];
+  $subjectList = json_decode($_POST["subject"], true);
+
+  if ($type === 'add') {
+    // new object
+    $newSubject = [];
+
+    // gen UUID
+    $newSubject['subjectID'] = gen_uuid();
+    
+    $newSubject['subjectTitle'] = "";
+
+    $newSubject['subjectList'] = [];
+
+    array_push($subjectList['subjects'], $newSubject);
+
+    // write back
+    file_put_contents($jsonFile_direct, json_encode($subjectList, JSON_UNESCAPED_UNICODE));
+    response('success', 'success');
+  }
+
+  function response($errorType, $message) {
+    $res = array('type' => $errorType, 'mesage' => $message);
+    echo json_encode($res, JSON_UNESCAPED_UNICODE);
+  }
+
+  // uuid V4
+  function gen_uuid() {
+    return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+      // 32 bits for "time_low"
+      mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+      // 16 bits for "time_mid"
+      mt_rand( 0, 0xffff ),
+
+      // 16 bits for "time_hi_and_version",
+      // four most significant bits holds version number 4
+      mt_rand( 0, 0x0fff ) | 0x4000,
+
+      // 16 bits, 8 bits for "clk_seq_hi_res",
+      // 8 bits for "clk_seq_low",
+      // two most significant bits holds zero and one for variant DCE1.1
+      mt_rand( 0, 0x3fff ) | 0x8000,
+
+      // 48 bits for "node"
+      mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+    );
+  }
+?>
