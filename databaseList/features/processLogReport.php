57987b7aaa39aa4333f4086e10fb188ccd654b32
tomcruihao <tomcruihao>
2020-02-25 17:25:36 +0800

    test

--- databaseList/features/processLogReport.php
+++ databaseList/features/processLogReport.php
@@ -23,6 +23,7 @@ error_reporting(E_ALL);
   $clickedData_filtered_by_date = [];
   foreach($logData['log'] as $log) {
     $logDateTime = strtotime($log['clickedDateTime']);
+    echo (strtotime($logDateTime) - strtotime($startTime->format('Y-m-d'))).(strtotime($logDateTime) - strtotime($endTime->format('Y-m-d'))).'<br>';
     if(strtotime($logDateTime) >= strtotime($startTime->format('Y-m-d')) && strtotime($logDateTime) <= strtotime($endTime->format('Y-m-d'))) {
       array_push($clickedData_filtered_by_date, $log);
     }
