4d300c285bdc5476051a9465532fdf492d55f2ab
tomcruihao <tomcruihao>
2020-02-25 10:35:54 +0800

    test

--- databaseList/features/processLogReport.php
+++ databaseList/features/processLogReport.php
@@ -6,9 +6,21 @@
   $getResourceData = file_get_contents('../data/eResourceList.json');
   $resourceData = json_decode($getResourceData, true);
   
-
   $getLogJsonData = file_get_contents('../data/logUserCountClick.json');
   $logData = json_decode($getLogJsonData, true);
+
+  // $startData = json_decode($_GET["startDate"], true);
+  $startTime = strtotime('2020-02-01');
+  $endTime = strtotime('2020-02-28');
+
+  // filter the log data
+  $clickedData_filtered_by_date = [];
+  foreach($logData['log'] as $log) {
+    $logDateTime = strtotime($log['clickedDateTime']);
+    if($logDateTime >= $startTime && $logDateTime <= $endTime) {
+      array_push($clickedData_filtered_by_date, $log);
+    }
+  }
 
   // create map
   $resourceIdArray = [];
@@ -19,7 +31,7 @@
 
   // create log counting array
   $clickCountingAry = [];
-  foreach($logData['log'] as $log) {
+  foreach($clickedData_filtered_by_date as $log) {
     // echo $resourceIdArray[$log['id']].' IP: '.$log['ip'];
     if (array_key_exists($log['id'], $clickCountingAry)) {
       $clickCountingAry[$log['id']]['clickTimes']++;
