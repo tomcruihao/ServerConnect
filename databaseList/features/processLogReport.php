1b301ced86685dbdc09c05d1002b58a3f5c1b618
tomcruihao <tomcruihao>
2020-02-25 11:02:23 +0800

    test

--- databaseList/features/processLogReport.php
+++ databaseList/features/processLogReport.php
@@ -1,4 +1,9 @@
 <?php
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
+
   header("Access-Control-Allow-Origin: *");
   header("Content-Security-Policy: upgrade-insecure-requests");
   header('Content-Type: application/json');
