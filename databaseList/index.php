891b78443fef272d6744b70fb4fda2fc82bf4eaf
tomcruihao <tomcruihao>
2020-03-02 18:13:16 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -50,7 +50,6 @@
     </nav>
   </header>
   <section>
-    <button onclick="change()">test</button>
     <div id="mainTitle" class="mainTitle">
       <h1 v-text="$t('message.h1_latestNews')"></h1>
       <div class="lang-wrap">
