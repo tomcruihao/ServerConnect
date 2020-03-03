58709923dd1829139221513fe5aabc2ac8635802
tomcruihao <tomcruihao>
2020-03-03 22:23:20 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -337,7 +337,7 @@
       setLang(event) {
         i18n.locale = event.target.value;
         localStorage.setItem('lang', event.target.value);
-        changeEsourceListLanguage();
+        // changeEsourceListLanguage();
 
         aside.setLocale(i18n.locale);
       }
