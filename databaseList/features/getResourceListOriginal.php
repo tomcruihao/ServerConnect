4b5d99aa8669d69638af6996839d499629a82928
Jay <tomcruihao@gmail.com>
2020-03-05 04:24:12 +0800

    test

--- databaseList/features/getResourceListOriginal.php
+++ databaseList/features/getResourceListOriginal.php
@@ -3,6 +3,19 @@
   header("Content-Security-Policy: upgrade-insecure-requests");
   header('Content-Type: application/json');
 
-  $getJsonData = file_get_contents('../data/eResourceList.json');
-  echo $getJsonData;
+  $jsonFile_direct = '../data/eResourceList.json';
+
+  // get resource list
+  $getResourceListJsonData = file_get_contents($jsonFile_direct);
+  $resourceList = json_decode($getResourceListJsonData, true);
+
+  $temp_en = [];
+  $temp_tw = [];
+
+  foreach($resourceList as $key => $value) {
+    print_r($value);
+    // $resourceList['en']
+  }
+
+  // echo $getJsonData;
 ?>
