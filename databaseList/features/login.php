88d0125b4ff7c20e3210eb2b8a499646474221ea
tomcruihao <tomcruihao>
2020-03-10 15:51:52 +0800

    test

--- databaseList/features/login.php
+++ databaseList/features/login.php
@@ -1,4 +1,9 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Security-Policy: upgrade-insecure-requests");
+  header('Content-Type: application/json');
+
+  
   foreach (getallheaders() as $name => $value) {
     echo "$name: $value\n";
   }
