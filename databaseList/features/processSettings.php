65c59282b8b7d90b677ae9aa28a63ff2c61a48fb
tomcruihao <tomcruihao>
2020-03-10 10:03:37 +0800

    test

---
+++ databaseList/features/processSettings.php
@@ -0,0 +1,24 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);
+
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Security-Policy: upgrade-insecure-requests");
+  header('Content-Type: application/json');
+
+  $jsonFile_direct = '../data/settings.json';
+
+  // parameters
+  $type = $_POST["type"];
+  // $resource = $_POST["resource"];
+  $recievedSettings = json_decode($_POST["settings"], true);
+
+  // get resource list
+  $getSettingJsonData = file_get_contents($jsonFile_direct);
+  $settings = json_decode($getSettingJsonData, true);
+
+  print_r($recievedSettings);
+
+?>
