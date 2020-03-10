3c5e09c5217c3140cc32b22a07766ce54349303b
tomcruihao <tomcruihao>
2020-03-10 15:52:43 +0800

    test

--- databaseList/features/login.php
+++ databaseList/features/login.php
@@ -3,11 +3,11 @@
   header("Content-Security-Policy: upgrade-insecure-requests");
   header('Content-Type: application/json');
 
-  
+
   foreach (getallheaders() as $name => $value) {
     echo "$name: $value\n";
   }
-　session_start();
-  $_SESSION['UserName']='Jordan';
-  echo $_SESSION['UserName'];
+// 　session_start();
+//   $_SESSION['UserName']='Jordan';
+//   echo $_SESSION['UserName'];
 ?>
