38b3c92a86fd2a48ed852ac9db821b22d3c4ea31
tomcruihao <tomcruihao>
2020-03-11 09:40:15 +0800

    test

--- databaseList/features/login.php
+++ databaseList/features/login.php
@@ -5,6 +5,7 @@ ini_set("html_errors", 1);
 error_reporting(E_ALL);
 
   header("Access-Control-Allow-Headers: *");
+  header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Allow-Origin: *");
   header("Content-Security-Policy: upgrade-insecure-requests");
   header('Content-Type: application/json');
@@ -13,8 +14,8 @@ error_reporting(E_ALL);
     echo "$name: $value\n";
   }
 
-  session_start();
-  echo $_SESSION['UserName'];
+  // session_start();
+  // echo $_SESSION['UserName'];
   if(isset($_SESSION['UserName'])) {
     echo 'session is exist';
     echo $_SESSION['UserName'];
