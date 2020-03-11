08f3b517307d4de7e32fd50dc744c6b9702516dd
tomcruihao <tomcruihao>
2020-03-11 10:38:16 +0800

    test

--- databaseList/features/login.php
+++ databaseList/features/login.php
@@ -8,19 +8,31 @@ error_reporting(E_ALL);
   header("Access-Control-Allow-Credentials: true");
   header("Access-Control-Allow-Origin: *");
   header("Content-Security-Policy: upgrade-insecure-requests");
-  header('Content-Type: application/json');
+  header('Content-Type: application/json;charset=UTF-8');
 
-  foreach (getallheaders() as $name => $value) {
-    echo "$name: $value\n";
-  }
+  $jsonFile_direct = '../data/u5er.json';
 
-  // session_start();
-  // echo $_SESSION['UserName'];
-  if(isset($_SESSION['UserName'])) {
-    echo 'session is exist';
-    echo $_SESSION['UserName'];
-  } else {
-    $_SESSION['UserName'] = 'Jordan';
-    echo $_SESSION['UserName'];
-  }
+  $getUserListJsonData = file_get_contents($jsonFile_direct);
+  $userList = json_decode($getUserListJsonData, true);
+
+  $received_user = json_decode($_POST["user"], true);
+  echo $received_user['password'];
+  echo md5($received_user['password']);
+
+  
+
+
+  // foreach (getallheaders() as $name => $value) {
+  //   echo "$name: $value\n";
+  // }
+
+  // // session_start();
+  // // echo $_SESSION['UserName'];
+  // if(isset($_SESSION['UserName'])) {
+  //   echo 'session is exist';
+  //   echo $_SESSION['UserName'];
+  // } else {
+  //   $_SESSION['UserName'] = 'Jordan';
+  //   echo $_SESSION['UserName'];
+  // }
 ?>
