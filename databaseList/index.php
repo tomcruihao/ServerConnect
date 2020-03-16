c36d9eb63e22a965a3bc2b192a1099e7b2477df1
tomcruihao <tomcruihao>
2020-03-17 01:09:34 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -7,6 +7,12 @@
   $getResourceListJsonData = file_get_contents($jsonFile_direct);
   $resourceList = json_decode($getResourceListJsonData, true);
 
+  // get Proxy
+  $getSettingJsonData = file_get_contents('../data/settings.json');
+  $settingData = json_decode($getSettingJsonData, true);
+
+  $proxy = $settingData['proxy'];
+
   $result = [];
   $result_en = [];
   $result_local = [];
@@ -32,6 +38,12 @@
     foreach($value['local'] as $tkey => $tValue) {
       $temp_local[$tkey] = $tValue;
     }
+
+    if($isProxy) {
+      $temp_en['resourceUrl'] = $proxy.$temp_en['resourceUrl'];
+      $temp_local['resourceUrl'] = $proxy.$temp_local['resourceUrl'];
+    }
+    
     array_push($result_en, $temp_en);
     array_push($result_local, $temp_local);
   }
