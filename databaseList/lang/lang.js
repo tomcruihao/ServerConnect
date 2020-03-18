db6b88661fa692f49d2335d210ff04d35c141140
tomcruihao <tomcruihao>
2020-03-18 18:03:58 +0800

    test

--- databaseList/lang/lang.js
+++ databaseList/lang/lang.js
@@ -82,6 +82,7 @@ const messages = {
       dialogue_content_expired: 'The credential has expired, please log in again',
       dialogue_content_updateSuccess: 'Update Successful',
       dialogue_content_account_updateSuccess: 'Your account has updated successfully, please re-login',
+      dialogue_content_logout: 'Log out successfully',
       // button
       btn_recover: 'Recover',
       btn_update: 'Update',
@@ -185,6 +186,7 @@ const messages = {
       dialogue_content_expired: '憑證已失效, 請重新登入',
       dialogue_content_updateSuccess: '更新成功',
       dialogue_content_account_updateSuccess: '帳號更新成功, 請重新登入',
+      dialogue_content_logout: '登出成功',
       // button
       btn_recover: '復原',
       btn_update: '更新',
