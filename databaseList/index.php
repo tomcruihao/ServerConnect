82a48b35a03bd998b6d5be2e785156da507dbf38
tomcruihao <tomcruihao>
2020-03-23 09:56:15 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -255,7 +255,7 @@
   // gtag('config', 'UA-XXXXX');
 
   // 
-  const GA_ID = <?php echo $GA_ID; ?>;
+  const GA_ID = '<?php echo $GA_ID; ?>';
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
