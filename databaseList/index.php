56add91b0b913be9c594597d18f809191ed0aff2
tomcruihao <tomcruihao>
2020-03-23 09:54:58 +0800

    test

--- databaseList/index.php
+++ databaseList/index.php
@@ -248,11 +248,23 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $GA_ID; ?>"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  // window.dataLayer = window.dataLayer || [];
+  // function gtag(){dataLayer.push(arguments);}
+  // gtag('js', new Date());
 
-  gtag('config', '<?php echo $GA_ID; ?>');
+  // gtag('config', 'UA-XXXXX');
+
+  // 
+  const GA_ID = <?php echo $GA_ID; ?>;
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ 
+
+  ga('create', GA_ID, 'auto');
+  ga('send', 'pageview');
 </script>
 
 <script type="text/javascript">
