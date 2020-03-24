c57ef95ab7532b2e779f09c04e7444d8d3c9ba30
tomcruihao <tomcruihao>
2020-03-24 09:55:59 +0800

    test

--- databaseList/features/fileConverter.php
+++ databaseList/features/fileConverter.php
@@ -5,11 +5,9 @@
 
   if ( 0 < $_FILES['file']['error'] ) {
     echo 'Error: ' . $_FILES['file']['error'] . '<br>';
-    echo "Error";
-  }
-  else {
-    move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
-    echo "Success";
+  } else {
+    // move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
+    echo $_FILES['file'];
   }
 
 // 
