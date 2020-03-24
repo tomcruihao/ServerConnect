8f009b69cd087da47a251d7b4c3ca296accddafd
tomcruihao <tomcruihao>
2020-03-24 10:00:45 +0800

    test

--- databaseList/features/fileConverter.php
+++ databaseList/features/fileConverter.php
@@ -6,7 +6,7 @@
   if ( 0 < $_FILES['file']['error'] ) {
     echo 'Error: ' . $_FILES['file']['error'] . '<br>';
   } else {
-    // move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
+    move_uploaded_file($_FILES['file']['tmp_name'], '../csvFiles/' . $_FILES['file']['name']);
     // echo $_FILES['file'];
     print_r($_FILES['file']);
   }
