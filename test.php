f777b7a079750ca3d62fbef08aae6448a9eeaec9
tomcruihao <tomcruihao>
2020-03-31 01:14:28 +0800

    test

--- test.php
+++ test.php
@@ -1,41 +1,28 @@
 <?php
-  function getRealurl($url) {
-    $ch = curl_init();
+  function get_proxy_site_page( $url )
+  {
+      $options = [
+          CURLOPT_RETURNTRANSFER => true,     // return web page
+          CURLOPT_HEADER         => true,     // return headers
+          CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+          CURLOPT_ENCODING       => "",       // handle all encodings
+          CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+          CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+          CURLOPT_TIMEOUT        => 120,      // timeout on response
+          CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+      ];
 
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+      $ch = curl_init($url);
+      curl_setopt_array($ch, $options);
+      $remoteSite = curl_exec($ch);
+      $header = curl_getinfo($ch);
+      curl_close($ch);
 
-    $html = curl_exec($ch);
-
-    $redirectedUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-
-    curl_close($ch);
-
-    return $redirectedUrl;
-
-
-    // $curl = curl_init();
-    // curl_setopt_array($curl, array(    
-    //   CURLOPT_URL => $url,
-    //   CURLOPT_HEADER => true,
-    //   CURLOPT_RETURNTRANSFER => true,
-    //   CURLOPT_NOBODY => true));
-
-    // $header = explode("\n", curl_exec($curl));
-    // curl_close($curl);
-
-    // if (strpos($header[0],'301') || strpos($header[0],'302')) {
-    //   if(is_array($header['Location'])) {
-    //     return $header['Location'][count($header['Location'])-1];
-    //   } else {
-    //     return $header['Location'];
-    //   }
-    // } else {
-    //   return $url;
-    // }
+      $header['content'] = $remoteSite;
+      return $header;
   }
-  $url = getRealurl('https://libermg.ncyu.edu.tw/cgi-bin/smartweaver/browse.cgi?ccd=2HOKxW&o=e0&s=c-1-114082');
+
+  $url = get_proxy_site_page('https://libermg.ncyu.edu.tw/cgi-bin/smartweaver/browse.cgi?ccd=bPrCxo&o=e0&s=c-1-230784');
   echo $url;
   // $jsonFilePath = './test.json';
   // $SID = $_GET['sid'];
