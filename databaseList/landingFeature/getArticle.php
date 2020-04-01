e20f7474132b56013d67a15f07c58dc4d2283216
tomcruihao <tomcruihao>
2020-04-02 00:38:15 +0800

    test

--- databaseList/landingFeature/getArticle.php
+++ databaseList/landingFeature/getArticle.php
@@ -14,8 +14,7 @@
     // set method and data
     curl_setopt($curl, CURLOPT_POST, 1);
     if ($data) {
-      // curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
     }
 
     // set header
