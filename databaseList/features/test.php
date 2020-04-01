378746bc0fabfa42676687dcbe1aeee6140e4ddc
tomcruihao <tomcruihao>
2020-04-01 09:04:24 +0800

    test

---
+++ databaseList/features/test.php
@@ -0,0 +1,23 @@
+<?php 
+  include('EDSapiFeatures/rest/EBSCOAPI.php');
+  /*On initializarion
+  * 
+  * Check token.txt file exist or not
+  * if not create a new file and call API Aithentication Method
+  * write authToken and timestamp into token.txt file
+  * 
+  */
+  $lockfile = fopen("lock.txt","w+");
+  fclose($lockfile);
+  if(file_exists("token.txt")) {
+
+  } else {
+    $tokenFile = fopen("token.txt","w+");
+    $api = new EBSCOAPI();
+    $result = $api->apiAuthenticationToken();
+    fwrite($tokenFile, $result['authenticationToken']."\n");
+    fwrite($tokenFile, $result['authenticationTimeout']."\n");
+    fwrite($tokenFile, $result['authenticationTimeStamp']);
+    fclose($tokenFile);
+  }
+?>
