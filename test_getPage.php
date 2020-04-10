e0f0f9b16a70fefa444d7d04bcd5362ce1f614b0
tomcruihao <tomcruihao>
2020-04-10 11:44:53 +0800

    test

--- test_getPage.php
+++ test_getPage.php
@@ -1,4 +1,4 @@
 <?php
-  $html = file_get_contents('http://stackoverflow.com/questions/ask');
+  $html = file_get_contents('https://catalog.xmu.edu.cn/opac/ajax_item.php?marc_no=0002119361');
   echo $html;
 ?>
