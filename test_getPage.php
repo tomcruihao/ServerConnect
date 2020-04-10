7eb5f1dc4e4cf95bc444e5eee50e2501663149c1
tomcruihao <tomcruihao>
2020-04-10 11:47:10 +0800

    test

--- test_getPage.php
+++ test_getPage.php
@@ -1,4 +1,7 @@
 <?php
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type:text/html; charset=utf-8");
+
   $html = file_get_contents('https://catalog.xmu.edu.cn/opac/ajax_item.php?marc_no=0002119361');
   echo $html;
 ?>
