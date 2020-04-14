25f4b69233fbd14aff13678e8f90eec6e01e8b3b
tomcruihao <tomcruihao>
2020-04-14 11:47:36 +0800

    test

--- huiwen/rtac_without_ssl.php
+++ huiwen/rtac_without_ssl.php
@@ -27,9 +27,7 @@
 
   function getContent($url) {
     $ch = curl_init();
-	$timeout = 0;
 	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 	curl_setopt($ch, CURLOPT_HEADER, TRUE);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
@@ -39,6 +37,8 @@
 
     $html = curl_exec($ch);
     echo $html;
+
+    curl_close($ch);
 
  //    $ch1 = curl_init();
  //    curl_setopt($ch1,CURLOPT_URL,$url);
