f0777d5ded9e7b1de263ec057588b1e89e143497
tomcruihao <tomcruihao>
2020-04-14 11:27:35 +0800

    test

--- huiwen/rtac_without_ssl.php
+++ huiwen/rtac_without_ssl.php
@@ -4,24 +4,6 @@
 
   $getOriginalUrl = $_GET['oriurl'];
 
-
-  // function getrealurl($url){
-  //   $header = @get_headers($url,1);  //默认第二个参数0，可选1，返回关联数组
-  //   if(!$header){
-  //     exit('无法打开此网站'.$url);
-  //   }
-  //   //var_dump($header);
-  //   if (strpos($header[0],'301') || strpos($header[0],'302')) {
-  //     if(is_array($header['Location'])) {
-  //       return $header['Location'][count($header['Location'])-1];
-  //     } else {
-  //       return $header['Location'];
-  //     }
-  //   } else {
-  //     return $url;
-  //   }
-  // }
-
   function getRealurl($url) {
     $ch = curl_init();
 	$timeout = 0;
@@ -37,32 +19,10 @@
     $html = curl_exec($ch);
 
     $redirectedUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
-    echo $redirectedUrl;
 
     curl_close($ch);
 
     return $redirectedUrl;
-
-
-    // $curl = curl_init();
-    // curl_setopt_array($curl, array(    
-    //   CURLOPT_URL => $url,
-    //   CURLOPT_HEADER => true,
-    //   CURLOPT_RETURNTRANSFER => true,
-    //   CURLOPT_NOBODY => true));
-
-    // $header = explode("\n", curl_exec($curl));
-    // curl_close($curl);
-
-    // if (strpos($header[0],'301') || strpos($header[0],'302')) {
-    //   if(is_array($header['Location'])) {
-    //     return $header['Location'][count($header['Location'])-1];
-    //   } else {
-    //     return $header['Location'];
-    //   }
-    // } else {
-    //   return $url;
-    // }
   }
 
   function getContent($url) {
@@ -77,16 +37,13 @@
     echo $response;
   }
 
-  // $url = 'http://opac.lib.nankai.edu.cn/api/itemgo.php?marc_no=0000930184&appid=eds&time=2019-06-2815:54:07&sign=0f5565e3fa910d1bd23959ebe4c7d172';
   $url = getRealurl($getOriginalUrl);
-  echo $url;
-
 
   $getDomainName = parse_url($url);
   $domainName = $getDomainName["host"];
   $port = $getDomainName["port"];
 
-
+  echo $url.'@@@@@';
   $url_extract = explode("=",$url);
   echo 'ID: '.$url_extract.'@@@';
 
