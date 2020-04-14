2263b2ab04b7f5134788d15b921de05f672dde17
tomcruihao <tomcruihao>
2020-04-14 11:45:27 +0800

    test

--- huiwen/rtac_without_ssl.php
+++ huiwen/rtac_without_ssl.php
@@ -26,15 +26,29 @@
   }
 
   function getContent($url) {
-    $ch1 = curl_init();
-    curl_setopt($ch1,CURLOPT_URL,$url);
-    curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
+    $ch = curl_init();
+	$timeout = 0;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    $response = curl_exec($ch1);
-    curl_close($ch1);
-    echo $response;
+
+    $html = curl_exec($ch);
+    echo $html;
+
+ //    $ch1 = curl_init();
+ //    curl_setopt($ch1,CURLOPT_URL,$url);
+ //    curl_setopt($ch1,CURLOPT_RETURNTRANSFER,1);
+ //    curl_setopt($ch1,CURLOPT_CONNECTTIMEOUT, 4);
+	// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+ //    $response = curl_exec($ch1);
+ //    curl_close($ch1);
+ //    echo $response;
   }
 
   $url = getRealurl($getOriginalUrl);
