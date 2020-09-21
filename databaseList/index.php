1d034c44a4d05b01e8721d53a24a3bd67ea615fc
tomcruihao <tomcruihao>
2020-09-21 18:34:29 +0800

    remove debug mode

--- databaseList/index.php
+++ databaseList/index.php
@@ -1,10 +1,5 @@
 <!DOCTYPE html>
 <?php
-  ini_set("display_errors", 1);
-  ini_set("track_errors", 1);
-  ini_set("html_errors", 1);
-  error_reporting(E_ALL);
-
   date_default_timezone_set('Asia/Taipei');
   $jsonFile_direct = 'data/eResourceList.json';
 
